7c1733a0f36540f03974a2fccd8c9ee7e340cb17
zxh804159854 <zxh804159854@163.com>
2024-01-26 19:13:15 +0800

    将openfresco的opensees相关文件合并过来
    通过本地脚本，更新对应的openseesCore和openseesExtra文件

---
+++ SRC/openseesCore/MaterialResponse.cpp
@@ -0,0 +1,126 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// $Revision: 1.4 $
+// $Date: 2007-03-02 00:12:50 $
+// $Source: /usr/local/cvs/OpenSees/SRC/recorder/response/MaterialResponse.cpp,v $
+                                                                        
+// Written: MHS 
+// Created: Oct 2000
+//
+// Description: This file contains the MaterialResponse class implementation
+
+#include <MaterialResponse.h>
+#include <Material.h>
+#include <MaterialState.h>
+
+MaterialResponse::MaterialResponse(Material *mat, int id):
+  Response(), theMaterial(mat), theMaterialState(0), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(Material *mat, int id, int val):
+  Response(val), theMaterial(mat), theMaterialState(0), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(Material *mat, int id, double val):
+  Response(val), theMaterial(mat), theMaterialState(0), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(Material *mat, int id, const ID &val):
+  Response(val), theMaterial(mat), theMaterialState(0), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(Material *mat, int id, const Vector &val):
+  Response(val), theMaterial(mat), theMaterialState(0), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(Material *mat, int id, const Matrix &val):
+  Response(val), theMaterial(mat), theMaterialState(0), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(MaterialState *mat, int id):
+  Response(), theMaterial(0), theMaterialState(mat), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(MaterialState *mat, int id, int val):
+  Response(val), theMaterial(0), theMaterialState(mat), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(MaterialState *mat, int id, double val):
+  Response(val), theMaterial(0), theMaterialState(mat), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(MaterialState *mat, int id, const ID &val):
+  Response(val), theMaterial(0), theMaterialState(mat), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(MaterialState *mat, int id, const Vector &val):
+  Response(val), theMaterial(0), theMaterialState(mat), responseID(id)
+{
+
+}
+
+MaterialResponse::MaterialResponse(MaterialState *mat, int id, const Matrix &val):
+  Response(val), theMaterial(0), theMaterialState(mat), responseID(id)
+{
+
+}
+
+MaterialResponse::~MaterialResponse()
+{
+
+}
+
+int
+MaterialResponse::getResponse(void)
+{
+  if (theMaterial != 0)
+    return theMaterial->getResponse(responseID, myInfo);
+  else if (theMaterialState != 0)
+    return theMaterialState->getResponse(responseID, myInfo);
+  else
+    return 0;
+}
+
+int
+MaterialResponse::getResponseSensitivity(int gradNumber)
+{
+  return theMaterial->getResponseSensitivity(responseID, gradNumber, myInfo);
+}
---
+++ SRC/openseesExtra/MaterialState.h
@@ -0,0 +1,60 @@
+/* ****************************************************************** **
+**    OpenSees - Open System for Earthquake Engineering Simulation    **
+**          Pacific Earthquake Engineering Research Center            **
+**                                                                    **
+**                                                                    **
+** (C) Copyright 1999, The Regents of the University of California    **
+** All Rights Reserved.                                               **
+**                                                                    **
+** Commercial use of this program without express permission of the   **
+** University of California, Berkeley, is strictly prohibited.  See   **
+** file 'COPYRIGHT'  in main directory for information on usage and   **
+** redistribution,  and for a DISCLAIMER OF ALL WARRANTIES.           **
+**                                                                    **
+** Developed by:                                                      **
+**   Frank McKenna (fmckenna@ce.berkeley.edu)                         **
+**   Gregory L. Fenves (fenves@ce.berkeley.edu)                       **
+**   Filip C. Filippou (filippou@ce.berkeley.edu)                     **
+**                                                                    **
+** ****************************************************************** */
+                                                                        
+// $Revision$
+// $Date$
+// $Source$
+
+// Written: MHS
+// Created: August 2000
+//
+// Description: This file contains the interface for MaterialState,
+// which is the base for classes which model states of hysteretic
+// degradation.
+
+#ifndef MaterialState_h
+#define MaterialState_h
+
+#include <DomainComponent.h>
+#include <MovableObject.h>
+
+class OPS_Stream;
+class Information;
+class Response;
+
+class MaterialState : public TaggedObject, public MovableObject
+{
+ public:
+  MaterialState(int tag, int classTag);    
+  virtual ~MaterialState();
+
+  virtual Response *setResponse(const char **argv, int argc, OPS_Stream &s);
+  virtual int getResponse(int responseID, Information &info);
+  
+  virtual int setVariable(const char *argv);
+  virtual int getVariable(int variableID, double &info);
+  
+ protected:
+  
+ private:
+
+};
+
+#endif
